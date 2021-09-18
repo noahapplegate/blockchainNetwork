bf5c84317418d0f19a39e7bd75f351724039b30f
Noah Applegate <noahapplegate1@gmail.com>
2021-09-18 12:52:39 -0700

    Update cryptoNetwork.py specifications

--- cryptoNetwork.py
+++ cryptoNetwork.py
@@ -1,22 +1,23 @@
-import transaction
+from blockchain import Blockchain, Block
+from transaction import Transaction, TXInput, TXOutput
 
 
 class Wallet:
     """
-    Facilitates the spending and receiving of coin
+    Facilitates the spending and receiving of coins
     ...
     Attributes
     ----------
-    secret_key : str
+    secret_key : bytes
         The Wallet's secret key
-    public_key : str
+    public_key : bytes
         The Wallet's public key. Acts as an address for other Wallet's to
         reference when sending coin to this Wallet
     balance : int
         Total amount of coin held by the Wallet
     node : FullNode
         The node in the cryptocurrency network this wallet is connected to
-    wallet_utxo_set: List[(txid, txout_ind, amount)]
+    wallet_utxo_set: List[(txid: bytes, output_ind: int, amount: int)]
         List of UTXOs and their corresponding amounts owned by this wallet
         UTXOs are referenced by a triple containing their Transaction ID,
         the output index, and the amount of coin within them
@@ -24,10 +25,10 @@ class Wallet:
 
     Methods
     -------
-    __init__(node)
+    __init__(node: FullNode)
         Generates a secret key and public key for this Wallet. Initializes
         the balance, UTXO set, and connects the Wallet to the given FullNode.
-    send(outputs)
+    send(outputs: List[TXOutput])
         Sends coin in amounts and locations specified by outputs which
         is a list of TXOutputs
     updateWallet()
@@ -38,8 +39,8 @@ class Wallet:
 
 class FullNode:
     """
-    A Node on the network used to validate transactions and
-    maintain a full copy of the Blockchain
+    A Node on the network used to validate transactions and maintain a full
+    copy of the Blockchain
     ...
     Attributes
     ----------
@@ -49,21 +50,22 @@ class FullNode:
         List of Transactions that have not yet been validated
     validated_txs : List[Transaction]
         List of validated Transactions to be broadcast to miners
-    utxo_set : Map: (transaction ID, index) -> TXOutput
-        Database of unsent transaction outputs
+    utxo_set : Dict[bytes, bytes]
+        Maps Transaction ID's of UTXOs to the public key that owns them
 
     Methods
     -------
     __init__()
-        Used to initialize the first FullNode on the network
-    __init__(node)
+        Used to initialize a new FullNode
+    copy(node: FullNode)
         Used to initialize subsequent FullNodes and gives them copies
-        of a currently running FullNode's Blockchain and UTXO set
-    validate_tx(tx)
-        Determines if a Transaction is valid. No double spends, over spends
-    listen_for_blocks(blocks):
-        Verifies transactions on blocks confirmed by miners and adds it to
-        the node's blockchain. Updates the UTXO set.
+        of an existing FullNode's Blockchain and UTXO set
+    validate_tx(tx: Transaction)
+        Determines if a Transaction is valid. Checks for double spends,
+        over spends, invalid signatures, invalid Transaction data.
+    listen_for_blocks(block: Block):
+        Verifies transactions and proof-of-work on a new Block confirmed by
+        miners and adds it to the node's blockchain. Updates the UTXO set.
     """
 
 
@@ -74,20 +76,20 @@ class MinerNode:
     Attributes
     ----------
     mempool : List[Transactions]
-        Verified transactions waiting to be added to a block
-    miner_public_key : Crypto.PublicKey.RSA
-        Miner's public key to be used for coinbase transactions and
-        to collect fees
+        Verified Transactions waiting to be added to a Block
+    miner_public_key : bytes
+        Miner's public key to be used for outputs of coinbase
+        Transactions and fees
 
     Methods
     -------
     __init__(miner_public_key)
         Initializes the miner_public_key and an empty mempool
-    listen_for_transactions(transactions)
-        Listen for transactions being broadcast and add them to the mempool
-        to await validation
+    listen_for_transactions(transactions: List[Transactions])
+        Listen for Transactions being broadcast and add them to the mempool
+        to await confirmation
     create_new_block()
-        Collect transactions from the mempool and add them to a new block
+        Collect transactions from the mempool and add them to a new Block
     proof_of_work(block)
         Perform a proof-of-work for the specified block
     """
